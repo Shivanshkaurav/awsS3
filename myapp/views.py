bdbdd84c58c8241a11657500ff98e4a799239738
shivansh <shivanshkaurav04@gmail.com>
2024-09-16 14:24:36 +0530

    index.html

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def index(request):
+    data = Student.objects.all()
+    context = {'data': data}
+    return render(request, 'index.html', context)
